649444280e786ee09055ad66ca673363bf486262
Alex Zhu <alexanderzhu07@gmail.com>
2024-11-12 10:45:09 -0500

    Adding logic for translation button and communicating with translation backend

---
+++ src/translate/index.js
@@ -0,0 +1,23 @@
+// // const request = require('request');
+
+// const translatorApi = module.exports;
+
+// translatorApi.translate = async function (postData) {
+// 	// Edit the translator URL below
+// 	const TRANSLATOR_API = 'https://codebb-translator-service.azurewebsites.net/';
+// 	const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
+// 	const data = await response.json();
+// 	return [data.is_english, data.translated_content];
+// };
+
+var request = require('request');
+
+const translatorApi = module.exports;
+
+translatorApi.translate = async function (postData) {
+    // Edit the translator URL below
+    const TRANSLATOR_API = "https://nodebb-f24-translator.azurewebsites.net/"
+    const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
+    const data = await response.json();
+    return [data["is_english"], data["translated_content"]]
+}
