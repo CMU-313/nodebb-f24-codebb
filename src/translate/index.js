4a82cefd9d52ef8de408bc50cd6de090dc83671f
Alex Zhu <alexanderzhu07@gmail.com>
2024-11-12 21:04:37 -0500

    Fixing tanslator_api link

--- src/translate/index.js
+++ src/translate/index.js
@@ -5,7 +5,7 @@ const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
 	// Edit the translator URL below
-	const TRANSLATOR_API = 'https://translator-service-team-team.azurewebsites.net/';
+	const TRANSLATOR_API = 'https://codebb-translator-service.azurewebsites.net/';
 	const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
 	const data = await response.json();
 	return [data.is_english, data.translated_content];
